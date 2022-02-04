4a7a454bb15d32da1120ad7b2559fa07de639b2a
aaltman99 <42359322+aaltman99@users.noreply.github.com>
2022-02-04 12:45:07 -0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,5 +8,7 @@
 module load python3
 export HDF5_USE_FILE_LOCKING=FALSE
 
-python3 $PATH-TO-SCRIPT/pseudobands.py --fname_in WFN_in.h5 --fname_in_q WFNq.h5 --fname_out WFN_SPB.h5 --fname_out_q WFN_SPB_q.h5 --nv 50 --nc 50 --efrac_v 0.01 --efrac_c_fine 0.01 --efrac_c 0.02 --max_freq 1.0 --nspbps_v 2 --nspbps_c 1 &
+# --uniform_width 0.00734 is in Ry. 0.00734 Ry == 100 meV
+
+python3 $PATH-TO-SCRIPT/pseudobands.py --fname_in WFN_in.h5 --fname_in_q WFNq.h5 --fname_out WFN_SPB.h5 --fname_out_q WFN_SPB_q.h5 --nv 50 --nc 50 --efrac_v 0.01 --uniform_width 0.00734 --efrac_c 0.02 --max_freq 1.0 --nspbps_v 1 --nspbps_c 1 --verbosity 1 &
 wait
