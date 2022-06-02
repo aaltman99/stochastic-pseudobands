ebbc57b0ddd6d11e580dbaa9843982e750dbff56
aaltman99 <42359322+aaltman99@users.noreply.github.com>
2022-06-02 11:33:15 -0700

    Update run.sh

--- scripts/optimized/run.sh
+++ scripts/optimized/run.sh
@@ -9,5 +9,9 @@
 module load python
 export HDF5_USE_FILE_LOCKING=FALSE
 
-python3 /pseudobands_opt.py --fname_in WFN.h5 --fname_in_q WFNq.h5 --fname_out WFN_SPB.h5 --fname_out_q WFN_SPB_q.h5 --nv 5 --nc 100 --nslice_v 5 --uniform_width 0.0 --nslice_c 20 --max_freq 0.0 --nspbps_v 1 --nspbps_c 1 --verbosity 2 &
-wait
+
+python3 ${PATH_TO_SCRIPT}/pseudobands_opt.py --fname_in WFN.h5 --fname_in_q WFNq.h5 --fname_out WFN_SPB.h5 --fname_out_q WFN_SPB_q.h5 --nv 1 --nc 1 --nslice_v 5 --nslice_c 175 --max_freq 0.0 --nspbps_v 5 --nspbps_c 5 --verbosity 2 
+
+
+# you can also just run this on the login node, since its a serial script
+# if you do, remove the HDF5_USE_FILE_LOCKING variable
